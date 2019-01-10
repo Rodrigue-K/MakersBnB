89e1fbaab0d209dd0e004f41b4ad1d9359f35be0
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-10 10:53:23 +0000

    User can sign up and be taken to profile page

--- app.rb
+++ app.rb
@@ -7,8 +7,23 @@ class MakersBnB < Sinatra::Base
   enable :method_override
 
   ENV['RACK_ENV'] ||= 'development'
-  
+
   get '/' do
     erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect '/profile'
+  end
+
+  get '/profile' do
+    @user = User.get(session[:user_id])
+    erb :profile
   end
 end
