5c7d885fe0e2e1d4882cef5a683526fdc915766e
Simon Cass <scass1991@gmail.com>
2019-01-10 11:17:52 +0000

    Skeletal listing page added

--- lib/list_space.rb
+++ lib/list_space.rb
@@ -1,9 +1,9 @@
 require 'data_mapper'
 
 class Space
-  include DataMapper::Resource
-  property :id, Serial
-  property :name, Text
-  property :price, Integer
-  property :description, Text
+  # include DataMapper::Resource
+  # property :id, Serial
+  # property :name, Text
+  # property :price, Integer
+  # property :description, Text
 end
