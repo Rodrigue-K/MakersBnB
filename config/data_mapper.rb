f8f1df5b09fba9e79f89408cbe6ba1f0323c3039
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2019-01-10 12:09:51 +0000

    Passing feature and unit tests for listing spaces. Database can persist data

--- config/data_mapper.rb
+++ config/data_mapper.rb
@@ -1,6 +1,7 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require './lib/user'
+require './lib/list_space'
 require 'data_mapper'
 
 DataMapper.setup(:default, "postgres://localhost/makers_bnb_#{ENV['RACK_ENV']}")
--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -1,14 +1,15 @@
-# require './lib/list_space'
-#
-# describe 'spaces' do
-#   space = Space.create(name:  "Makers", price: 435, description: "Nice")
-#   it 'has a name' do
-#     expect(space.name).to eq('Makers')
-#   end
-#   it 'has a price' do
-#     expect(space.price).to eq(435)
-#   end
-#   it 'has a description' do
-#     expect(space.description).to eq('Nice')
-#  end
-# end
+require 'list_space'
+
+describe Space do
+  let!(:space) { Space.create(name: "Makers", price: 435, description: "Nice") }
+
+  it 'has a name' do
+    expect(space.name).to eq( 'Makers')
+  end
+  it 'has a price' do
+    expect(space.price).to eq(435)
+  end
+  it 'has a description' do
+    expect(space.description).to eq('Nice')
+  end 
+end
