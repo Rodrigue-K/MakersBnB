aa81c50642b3050b714b60fde0f8d2fa194a0425
Simon Cass <scass1991@gmail.com>
2019-01-10 10:29:55 +0000

    Fixed RSpec & branch issues

--- app.rb
+++ app.rb
@@ -1,17 +1,13 @@
 require 'pry'
 require 'sinatra/base'
-<<<<<<< HEAD
 require './config/data_mapper'
-=======
-require '.config/data_mapper'
->>>>>>> 9bf3d691c8214854634be16f34f364d5c1de8995
 
 class MakersBnB < Sinatra::Base
   enable :sessions
   enable :method_override
 
   ENV['RACK_ENV'] ||= 'development'
-  
+
   get '/' do
     erb :index
   end
--- spec/features/infrastructure_spec.rb
+++
@@ -1,9 +0,0 @@
-feature Spacelisting do
-  scenario "user can see a listed space" do
-    visit "/"
-    expect(page).to have_content("Price")
-    expect(page).to have_content("Name")
-    expect(page).to have_content("Description")
-    expect(page).to have_content("Availability")
-  end
-end
