7413af65ca789979122a4f0d98d5ac0708f9745e
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-11 15:40:56 +0000

    Manual fix of merge issues

--- spec/features/availability_spec.rb
+++ spec/features/availability_spec.rb
@@ -6,11 +6,18 @@ feature 'Availability' do
     fill_in_bee_listing
   end
 
-  scenario 'A user can see the individual dates available to book' do
-    visit '/'
-    click_button 'Book'
-    expect(page).to have_link '2019-01-17'
-    expect(page).to have_link '2019-01-18'
-    expect(page).to have_link '2019-01-19'
+  scenario 'A user can see a booking page' do
+    first("#indv_space").click_button 'Book'
+
+    expect(page).to have_content 'When are you buzzing in?'
+    expect(page).to have_content 'Dates available'
   end
+
+  # scenario 'A user can see the individual dates available to book' do
+  #   visit '/'
+  #   click_button 'Book'
+  #   expect(page).to have_link '2019-01-17'
+  #   expect(page).to have_link '2019-01-18'
+  #   expect(page).to have_link '2019-01-19'
+  # end
 end
