cad138d8ae7d5c9183d5355fce29fe3b3287b7c0
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-11 15:14:49 +0000

    Create indivdual listing pages

--- spec/features/availability_spec.rb
+++ spec/features/availability_spec.rb
@@ -1,5 +1,16 @@
 feature 'Availability' do
   let!(:user) { User.create(email: 'test@test.com', password: 'testpassword') }
-  background { signin }
 
+  background do
+    signin
+    fill_in_bee_listing
+  end
+
+  scenario 'A user can see the individual dates available to book' do
+    visit '/'
+    click_button 'Book'
+    expect(page).to have_link '2019-01-17'
+    expect(page).to have_link '2019-01-18'
+    expect(page).to have_link '2019-01-19'
+  end
 end
